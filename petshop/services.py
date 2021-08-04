482516318d93d126904d2f91653b43d8e9f249fb
DoRenato <b4g4c31r0@gmail.com>
2021-08-04 04:58:29 -0300

    9

--- petshop/services.py
+++ petshop/services.py
@@ -2,8 +2,9 @@ import requests
 from requests.api import get
 
 
+url = 'https://qflashpetshop.herokuapp.com'
+
 def get_pets():
-    url = 'https://qflashpetshop.herokuapp.com'
     r = requests.get("{}/pet".format(url))
     pets = r.json()
     return pets
